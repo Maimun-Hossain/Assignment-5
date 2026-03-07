12add6dfa979ec68832124769779d7d66b218428
Maimun Hossain <maimun78l@gmail.com>
2026-03-07 07:24:42 +0300

    added sign in section

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+// console.log(document);
+const signInCard = document.getElementById("signInCard");
+const usernameInput = document.getElementById("usernameInput");
+const passwordInput = document.getElementById("passwordInput");
+// 
+const mainSection = document.getElementById("mainSection");
+
+function signInBtnFunc(id){
+    // console.log(usernameInput.value);
+    // console.log(passwordInput.value);
+
+    if(usernameInput.value === "admin" && passwordInput.value === "admin123"){
+        // console.log("Bingo");
+        signInCard.classList.add("hidden");
+        mainSection.classList.remove("hidden");
+    }
+}
