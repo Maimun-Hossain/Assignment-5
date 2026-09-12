8a991212be8a1dc332c7a74931b9974b6f9721ab
Maimun-Hossain <maimun78l@gmail.com>
2026-09-13 03:16:32 +0600

    Added Tech Section including everything

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { useState } from "react";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import Technologies from "./components/technologies/Technologies";
-// import Banner from './components/banner/Banner'
-// import Footer from './components/footer/Footer'
-// import Nav from './components/nav/Nav'
+import type { techData } from "./types";
+import Banner from './components/banner/Banner'
+import Footer from './components/footer/Footer'
+import Nav from './components/nav/Nav'
 
 const dataFetch = async () => {
   const response = await fetch("/data.json");
@@ -10,15 +14,18 @@ const dataFetch = async () => {
   return data;
 };
 const data = dataFetch();
+
 function App() {
+  const [cart, setCart] = useState<techData[]>([]);
   return (
     <>
-      {/* <Nav></Nav> */}
-      <div className="md:max-w-[85%] md:mx-auto">{/* <Banner></Banner> */}</div>
-      {/* <Footer></Footer> */}
+      <Nav></Nav>
+      <div className="md:max-w-[85%] md:mx-auto"><Banner></Banner></div>
       <div className="md:max-w-[85%] md:mx-auto my-20">
-        <Technologies data={data}></Technologies>
+        <Technologies data={data} cart={cart} setCart={setCart}></Technologies>
       </div>
+      <Footer></Footer>
+      <ToastContainer/>
     </>
   );
 }
--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,62 +1,181 @@
 import { use } from "react";
 import type { techData } from "../../types";
+import star from "../../assets/Symbol.png";
+import cross from "../../assets/xmark.png";
+import { toast } from "react-toastify";
 
 interface TechnologiesProps {
   data: Promise<techData[]>;
+  cart: techData[];
+  setCart: React.Dispatch<React.SetStateAction<techData[]>>;
 }
 
-const Technologies = ({ data }: TechnologiesProps) => {
+const Technologies = ({ data, cart, setCart }: TechnologiesProps) => {
   const textColor = { color: "#94a3b8FF" };
   const datas = use(data);
-  console.log(datas);
+  const handleAddToCart = (item: techData) => {
+    if (!cart.some((cartItem) => cartItem.id === item.id)) {
+      setCart([...cart, item]);
+      toast.success(`${item.name} added to stack!`);
+    }
+  };
+
+  const isInCart = (itemId: string) =>
+    cart.some((cartItem) => cartItem.id === itemId);
+
+  const handleRemoveFromCart = (id: string) => {
+    setCart((currentCart) => currentCart.filter((item) => item.id !== id));
+    toast.error(
+      `${cart.find((item) => item.id === id)?.name} removed from stack!`,
+    );
+  };
+
+  const handleRemoveAll = () => {
+    setCart([]);
+    toast.error("All technologies removed from stack!");
+  };
+  // console.log(datas);
+  const badgeStyle: Record<string, string> = {
+    Popular: "bg-[#e0f2feFF] text-[#0ea5e9FF]",
+    Versatile: "bg-[#dbeafeFF] text-[#2563ebFF]",
+    Fast: "bg-[#ffedd5FF] text-[#ea580cFF]",
+    Cache: "bg-[#fee2e2FF] text-[#dc2626FF]",
+    Standard: "bg-[#f3e8ffFF] text-[#7e22ceFF]",
+    Essential: "bg-[#fef3c7FF] text-[#ca8a04FF]",
+    Container: "bg-[#d1fae5FF] text-[#059669FF]",
+  };
   return (
     <div className=" px-2 lg:px-0">
-      <h2 className="font-extrabold text-4xl">
+      <h2 className="font-extrabold text-4xl text-center md:text-start">
         Explore the{" "}
         <span className="text-transparent bg-clip-text bg-[linear-gradient(to_bottom,rgba(216,27,126,1)_100%,rgba(124,58,237,1)_0%)]">
           Technologies
         </span>
       </h2>
-      <p className="text-lg mb-10" style={textColor}>
+      <p className="text-lg mb-10 text-center md:text-start" style={textColor}>
         Pick one technology per category to build your ideal stack.
       </p>
 
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
-        <div className="lg:col-span-9 bg-amber-100">
-          <div className="grid grid-cols-3 gap-3">
+        <div className="lg:col-span-9">
+          <div className="grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3">
             {datas.map((items) => {
               return (
-                <div className="card bg-base-100 shadow-sm">
-  <div className="card-body">
-    <span className="badge badge-xs badge-warning">Most Popular</span>
-    <div className="flex justify-between">
-      <h2 className="text-3xl font-bold">Premium</h2>
-      <span className="text-xl">$29/mo</span>
-    </div>
-    <ul className="mt-6 flex flex-col gap-2 text-xs">
-      <li>
-        <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
-        <span>High-resolution image generation</span>
-      </li>
-    </ul>
-    <div className="mt-6">
-      <button className="btn btn-neutral rounded-lg btn-block">Add to Stack</button>
-    </div>
-  </div>
-</div>
+                <div className="card border-2 border-[#E2E8F0]" key={items.id}>
+                  <div className="card-body">
+                    <div className="flex justify-between">
+                      <img
+                        className="w-10 h-10"
+                        src={items.icon}
+                        alt={items.name}
+                      />
+                      <span
+                        className={`badge badge-sm rounded-3xl ${badgeStyle[items.badge] || "bg-[#f1f5f9CC] text-[#1e293b]"}`}
+                      >
+                        {items.badge}
+                      </span>
+                    </div>
+                    <div className="flex justify-between">
+                      <h2 className="text-2xl font-bold">{items.name}</h2>
+                    </div>
+                    <p className="text-sm" style={textColor}>
+                      {items.description}
+                    </p>
+                    <div className="flex justify-between items-center mt-4">
+                      <div className="badge bg-[#f1f5f9CC] rounded-sm">
+                        {items.category}
+                      </div>
+                      <span style={textColor}>{items.difficulty}</span>
+                      <div className="flex items-center gap-1">
+                        <img src={star} alt="Star" />
+                        <span>{items.rating}</span>
+                      </div>
+                    </div>
+                    <div className="mt-6">
+                      <button
+                        onClick={() => handleAddToCart(items)}
+                        disabled={isInCart(items.id)}
+                        className={`btn rounded-lg btn-block ${
+                          isInCart(items.id)
+                            ? "btn-success text-white cursor-not-allowed"
+                            : "btn-neutral"
+                        }`}
+                      >
+                        {isInCart(items.id)
+                          ? "✓ Added to Stack"
+                          : "Add to Stack"}
+                      </button>
+                    </div>
+                  </div>
+                </div>
               );
             })}
           </div>
         </div>
         <div className="lg:col-span-3">
-          <div className=" rounded-2xl border-2 border-[#E2E8F0] p-4">
+          <div className="rounded-2xl border-2 border-[#E2E8F0] p-4">
             <h3 className="font-bold text-lg">Your Stack</h3>
-            <p className="text-sm" style={textColor}>
-              No technologies selected yet.
-            </p>
-            <div className="py-6 px-16 outline-2 outline-offset-2 outline-dashed outline-[#E2E8F0] rounded-2xl text-center my-4">
-              <p style={textColor}>Your stack is empty.</p>
-            </div>
+
+            {cart.length > 0 ? (
+              // If there are items in the cart then display
+              <>
+                <p style={textColor} className="text-sm">
+                  {cart.length} Technology Selected
+                </p>
+
+                <div className="mt-4 space-y-2">
+                  {cart.map((item) => (
+                    <div
+                      key={item.id}
+                      className="flex items-center justify-between rounded-xl border-2 border-[#E2E8F0] p-3"
+                    >
+                      <div className="flex items-center gap-3">
+                        <div className="flex items-center justify-center">
+                          <img
+                            src={item.icon}
+                            alt={item.name}
+                            className="h-7 w-7"
+                          />
+                        </div>
+                        <div>
+                          <p className="text-xs font-bold">{item.name}</p>
+                          <p
+                            className="text-[10px] font-semibold"
+                            style={textColor}
+                          >
+                            {item.category}
+                          </p>
+                        </div>
+                      </div>
+
+                      <button
+                        onClick={() => handleRemoveFromCart(item.id)}
+                        className="cursor-pointer"
+                      >
+                        <img src={cross} alt="Remove" className="h-5 w-5" />
+                      </button>
+                    </div>
+                  ))}
+                </div>
+
+                <button
+                  onClick={handleRemoveAll}
+                  className="mt-4 w-full rounded-xl border border-[#ed8c85FF] py-2 text-sm font-semibold text-[#d82c20FF] cursor-pointer hover:bg-[#d82c20FF] hover:text-white"
+                >
+                  Remove All
+                </button>
+              </>
+            ) : (
+              // If there are no items in the cart then display
+              <>
+                <p className="text-sm" style={textColor}>
+                  No technologies selected yet.
+                </p>
+                <div className="py-4 outline-2 outline-offset-2 outline-dashed outline-[#E2E8F0] rounded-2xl text-center my-4">
+                  <p style={textColor}>Your stack is empty.</p>
+                </div>
+              </>
+            )}
           </div>
         </div>
       </div>
